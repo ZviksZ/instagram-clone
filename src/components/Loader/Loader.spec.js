e0dcda7b3d50e504b08bbd3fa085cb1b3aadcf84
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2020-09-06 16:11:39 +0300

    eslint + prettier add to project

--- src/components/Loader/Loader.spec.js
+++ src/components/Loader/Loader.spec.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Loader} from './Loader';
 
+// eslint-disable-next-line
 const setUp = () => shallow(<Loader />);
 
 describe("Loader component", () => {
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,64 +1,39 @@
 export function getCookie(name) {
-   var matches = document.cookie.match(new RegExp("(?:^|; )" +
-      name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g,
-         '\\$1') +
-      "=([^;]*)"));
-   return matches ?
-      decodeURIComponent(matches[1]) :
-      undefined;
+	var matches = document.cookie.match(new RegExp('(?:^|; )' + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + '=([^;]*)'))
+	return matches ? decodeURIComponent(matches[1]) : undefined
 }
 
 export function setCookie(name, value, options) {
-   options =
-      options || {};
+	options = options || {}
 
-   var expires = options.expires;
+	var expires = options.expires
 
-   if (typeof expires ==
-      "number" &&
-      expires) {
-      var d = new Date();
-      d.setTime(d.getTime() +
-         expires *
-         1000);
-      expires =
-         options.expires =
-            d;
-   }
-   if (expires &&
-      expires.toUTCString) {
-      options.expires =
-         expires.toUTCString();
-   }
+	if (typeof expires == 'number' && expires) {
+		var d = new Date()
+		d.setTime(d.getTime() + expires * 1000)
+		expires = options.expires = d
+	}
+	if (expires && expires.toUTCString) {
+		options.expires = expires.toUTCString()
+	}
 
-   value =
-      encodeURIComponent(value);
+	value = encodeURIComponent(value)
 
-   var updatedCookie = name +
-      "=" +
-      value;
+	var updatedCookie = name + '=' + value
 
-   for (var propName in
-      options) {
-      updatedCookie +=
-         "; " +
-         propName;
-      var propValue = options[propName];
-      if (propValue !==
-         true) {
-         updatedCookie +=
-            "=" +
-            propValue;
-      }
-   }
+	for (var propName in options) {
+		updatedCookie += '; ' + propName
+		var propValue = options[propName]
+		if (propValue !== true) {
+			updatedCookie += '=' + propValue
+		}
+	}
 
-   document.cookie =
-      updatedCookie;
+	document.cookie = updatedCookie
 }
 
 export function deleteCookie(name) {
-   setCookie(name,
-      "", {
-         expires: -1
-      })
+	setCookie(name, '', {
+		expires: -1,
+	})
 }
